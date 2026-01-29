31daafc5e8b597c5a9c587ff1ca5d44c5ad3c720
Krishnatheja Vanka <theja.vanka@gmail.com>
2026-01-29 05:54:23 +0530

    add: Github actions for PyPI and TestPyPI

--- src/autotimm/__init__.py
+++ src/autotimm/__init__.py
@@ -1,6 +1,5 @@
 """AutoTimm: automated deep learning image tasks powered by timm and PyTorch Lightning."""
 
-from autotimm._version import __version__
 from autotimm.backbone import BackboneConfig, create_backbone, list_backbones
 from autotimm.data.datamodule import ImageDataModule
 from autotimm.heads import ClassificationHead
@@ -10,7 +9,6 @@ from autotimm.trainer import create_trainer
 from autotimm.utils import count_parameters
 
 __all__ = [
-    "__version__",
     "BackboneConfig",
     "ClassificationHead",
     "ImageClassifier",
